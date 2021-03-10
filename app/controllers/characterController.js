b99a38c1fd74d53e4512d43f267e8c829ec764fc
Jack-Jerry-Stark <jerry.estelle.dumont@gmail.com>
2021-03-10 19:47:50 +0100

    probleme de routage characters model

--- app/controllers/characterController.js
+++ app/controllers/characterController.js
@@ -1,54 +1,76 @@
 const Character = require('../models/Character');
 
 const characterController = {
-    // méthode pour la page d'accueil
-  characters: (request, response) => {
-    // on appelle la méthode pour récupérer les characters,
-    // Et on définit le callback, avec une erreur potentielle, et les characters demandées
-    Character.getAllCharacters( (err, characters) => {
-      // traitement d'erreur
-      if (err) {
-        console.error(err);
-        // on affiche l'erreur, telle qu'elle, dans le navigateur
-        response.status(500).send(err);
-      } else {
+  
+  characters: async (req, res) => {
+    // on fait appel à notre modèle
+    const characters = await Character.getAllCharacters();
 
-        // pas d'erreur => on envoie les character dans la view !
-        response.render('characters', {characters} );
-      }
-    });
-
+    // et on retourne le tout au client
+    res.render('characters');
   },
 
-  // méthode pour la page article
-  character: (request, response, next) => {
-    // on récupère l'id de l'article cible dans l'url
-    const characterId = request.params.id;
+  character: async (req, res) => {
+    const character = await Character.getCharacterById(req.params.id);
 
-    // on va cherhcer l'info dans la bdd
-    Character.getCharacterById(characterId, (err, character) => {
-      // traitement d'erreur
-      if (err) {
-        console.error(err);
-        // on affiche l'erreur, telle qu'elle, dans le navigateur
-        response.status(500).send(err);
-      } else {
-
-        // on oublie pas le traitement des 404 ! 
-        if (!character) {
-          next();
-        } else {
-          // on a les infos, on les transmet à la view
-          response.render('character', {character});
-        }
-
-      }
-    });
-  },
-    // une méthode pour gérer les 404
-    notFound: (req, res) => {
-      res.status(404).render('404');
+    if (character) {
+        res.render('character');
+    } else {
+        res.status(404).json('no such character');
     }
+
+    
+  },
+
+
+
+
+    // autre version
+    // méthode pour la page d'accueil
+  // characters: (request, response) => {
+  //   // on appelle la méthode pour récupérer les characters,
+  //   // Et on définit le callback, avec une erreur potentielle, et les characters demandées
+  //   Character.getAllCharacters( (err, characters) => {
+  //     // traitement d'erreur
+  //     if (err) {
+  //       console.error(err);
+  //       // on affiche l'erreur, telle qu'elle, dans le navigateur
+  //       response.status(500).send(err);
+  //     } else {
+
+  //       // pas d'erreur => on envoie les character dans la view !
+  //       response.render('characters', {characters} );
+  //     }
+  //   });
+
+  // },
+
+  // // méthode pour la page article
+  // character: (request, response, next) => {
+  //   // on récupère l'id de l'article cible dans l'url
+  //   const characterId = request.params.id;
+
+  //   // on va cherhcer l'info dans la bdd
+  //   Character.getCharacterById(characterId, (err, character) => {
+  //     // traitement d'erreur
+  //     if (err) {
+  //       console.error(err);
+  //       // on affiche l'erreur, telle qu'elle, dans le navigateur
+  //       response.status(500).send(err);
+  //     } else {
+
+  //       // on oublie pas le traitement des 404 ! 
+  //       if (!character) {
+  //         next();
+  //       } else {
+  //         // on a les infos, on les transmet à la view
+  //         response.render('character', {character});
+  //       }
+
+  //     }
+  //   });
+  // },
+    
 
 };
   
--- app/models/Character.js
+++ app/models/Character.js
@@ -1,61 +1,57 @@
 //const tableName = 'character';
 const client = require('../database');
 
-// const Character = {
-//     getAllCharacters: (callback) => {
-//         // 1. On écrit la requête
-//         const sql = `SELECT * FROM character`;
+const Character = {
+    getAllCharacters: (callback) => {
+        // 1. On écrit la requête
+        const sql = `SELECT * FROM character`;
 
-//         // 2. On exécute la requête
-//         client.query( sql, (err, data) => {
-//             // 3. passer le resultat au callback
-//             const characters = data.rows;
-//             callback(err, characters );
-//         });
+        // 2. On exécute la requête
+        client.query( sql, (err, data) => {
+            // 3. passer le resultat au callback
+            const characters = data.rows;
+            callback(err, characters );
+        });
+    },
 
-//         // getAllCharacters: async () => {
-//         //     // 1. On écrit la requête
-//         //     const sql = await client.query(`SELECT * FROM character`);
-//         //     return sql.rows;
-//     },
-
-// getCharacterById: (id, callback) => {
-//     const sql = `SELECT * FROM "character" WHERE "id" = $1;`;
-//     const values = [id];
-//     client.query( sql, values, (err, data) => {
-//         // 3. transmetrte les infos au callback
-//         if (err) {
-//             console.log(err);
+    getCharacterById: (id, callback) => {
+        const sql = `SELECT * FROM "character" WHERE "id" = $1;`;
+        const values = [id];
+        client.query( sql, values, (err, data) => {
+            // 3. transmetrte les infos au callback
+            if (err) {
+                console.log(err);
+            }
+            const character = data.rows[0];
+            callback( err, character );
+        });
+}
+// class Character {
+//     constructor(data = {}) {
+//         //super(data);
+//         for (const prop in data) {
+//             this[prop] = data[prop];
 //         }
-//         const character = data.rows[0];
-//         callback( err, character );
-//     });
-// }
-class Character {
-    constructor(data = {}) {
-        //super(data);
-        for (const prop in data) {
-            this[prop] = data[prop];
-        }
-      }
-        static async getAllCharacters(callback) {
-            try{  
-                const sql =  await client.query(`SELECT * FROM character;`);
-                  return sql.rows;
-                } catch (error) {
-                  console.log(error);
-                  res.status(500).render('500');
-                }
-              }
-              static async getCharacterById(characterId) {
-                try{  
-                    const character =  await client.query(`SELECT * FROM character WHERE "id" = $1;`, [characterId]);
-                    return character.rows[0];
-                    } catch (error) {
-                      console.log(error);
-                      res.status(500).render('500');
-                    }
-              }
+//       }
+//         static async getAllCharacters(callback) {
+//             try{  
+//                 const sql =  await client.query(`SELECT * FROM character`);
+//                   return sql.rows;
+//                 } catch (error) {
+//                   console.log(error);
+//                   res.status(500).render('500');
+//                 }
+//               }
+
+//         static async getCharacterById(characterId) {
+//             try{  
+//                 const character =  await client.query(`SELECT * FROM character WHERE "id" = $1`, [characterId]);
+//                 return character.rows[0];
+//                 } catch (error) {
+//                     console.log(error);
+//                     res.status(500).render('500');
+//                 }
+//               }
     
     // getTypes: (callback) => {
     //     // on execute qui prend un certain temps
